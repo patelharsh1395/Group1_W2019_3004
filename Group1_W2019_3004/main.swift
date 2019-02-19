5cb3dae1b09bd37b43bb7d73ef6806d89152ca34
Harsh <harsh@Harshs-Mac.local>
2019-02-19 11:05:20 -0800

    Initial Commit

---
+++ Group1_W2019_3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Group1_W2019_3004
+//
+//  Created by Harsh on 2019-02-19.
+//  Copyright © 2019 Harsh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
